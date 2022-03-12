bf6d0f9e1529554978efb91787c308feb9b6f6c6
Jose Palacios <jpalacios.barreda@gmail.com>
2022-03-12 16:21:11 -0700

    Catching up baseline
    Adding jupyter-notebook and new boundary condition code

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # sapflow
-
 PDE solver for soil infiltration problem with root water uptake term (Richard's equation)
 
--- sapflow.py
+++ sapflow.py
@@ -13,7 +13,7 @@ warnings.filterwarnings('error')
 
 # Van Genuchten constitutive relations
 @njit
-def Cw(h): 
+def Cw(h):
     return -α*n*m*(1 + (α*h)**n)**(-(m+1)) * (α*h)**(n-1)
 
 @njit
@@ -30,35 +30,39 @@ def invθ(θ): # h
 
 # Solver settings
 # ---------------
-sol_do_calibration_workflow = True   
+sol_do_calibration_workflow = False
 sol_compute_residuals = True
 sol_compute_numerical_probes = True
-sol_datafile = 'DF27_period1_reduced.csv' # 'test_data.csv'
+sol_datafile = './field_measurements/DF27_period1_reduced.csv' # 'test_data.csv'
 
 sol_use_RWU = False
-sol_use_free_drainage_bc = True #Boundary ~pp190
+RWU_start_hour = 8
+RWU_end_hour = 23
+sol_use_free_drainage_bc = True # Soil physics with Hydrus ~pp190
 
 # Simulation parameters
 # ---------------------
-θr = 0.01 #0.01
-θs = 0.539 #value for vertical fracture fillings karst (Yang)
+θr = 0.01
+θs = 0.539 # Value for vertical fracture fillings karst (Yang)
 θdry = 0.01 + 0.0001
 gbc = 0.1 # Constant gradient at lower boundary (L/L)
 m  = 0.99
 n  = 1/(1 - m)
-#n = 1.055 #soil physics w py
 #m = 1-(1/n)
 l = 1
-Ks = 1e-4
-α = -4.9e-4 #units m-1
+Ks = 1e-6
+α = -1#-4.9e-4
 Z = 200/1000
-
 τ0 = 0
-τ = 3600*24 #total time of simulation *24 is a whole day 
+τ = 3600*24 # total time of simulation *24 is a whole day
 t_checkpoints = [n*τ/8 for n in range(8+1)]
 dt = 0.75
 dz = Z/100
-Nz = int(Z/dz) #depth of fracture numero de cajitas 
+Nz = int(Z/dz) # grid cell count
+
+# Surface boundary conditions parameters
+a1 = 0.0003
+a2 = 0.002
 
 # Root water uptake
 # -----------------
@@ -72,7 +76,7 @@ C = 1e-6
 # Numerical probes
 # ----------------
 np_probe_every_s = 60 # s
-np_probe_depth = 25 # dz = 2mm
+np_probe_depth = 25 # dz = 25mm
 np_surface_head = []
 np_residual_median = []
 np_residual_max = []
@@ -82,13 +86,9 @@ np_dΨx = []
 print(f'>> dt={round(dt,4)}s, dz={round(dz,4)}m, Nz={Nz}')
 
 # Initial conditions
-h0 = np.zeros(Nz+2) 
-#h0[0] = invθ(0.4) #inversa de Van genuchte 
-#h0[1:-1] = invθ(0.4) #agarro elementos intermedios, puedo saturar todo el perfil 
-#h0[-1] = invθ(0.4) #cambiar aca si quiero local water table, neumann es free drainage θdry
-
-h0[0:5] = invθ(0.4) #inversa de Van genuchte 
-h0[5:]= invθ(θdry) #θdry
+h0 = np.zeros(Nz+2)
+h0[0] = invθ(0.4)
+h0[1:]= invθ(θdry)
 
 @njit
 def log_mean_p(X):
@@ -110,20 +110,27 @@ def log_mean_m(X):
             new[i] = X[i]
     return new
 
-def compute_RWU(h, Ψx):
-    _K = K(h[1:-1])
-    Rp = Rmin*np.exp((-Ψx/d)**Brwu)
-    λ = 1/(sum([β**(i*dz/Z) for (i,_) in enumerate(h[1:-1])]))
-    Rr = np.array([Rp/(λ*β**(i*dz/Z)) for (i,_) in enumerate(h[1:-1])])
-    Rsr = np.array([dz/(λ*β**(i*dz/Z)*_K[i]) for (i,_) in enumerate(h[1:-1])])
-    RWU = (h[1:-1] - Ψx)/(Rr + Rsr)
-    return RWU
+def compute_RWU(h, Ψx, timestamp):
+    if timestamp != None:
+        timestamp = int(f'{timestamp}'.split('T')[1].split(':')[0])
 
-def create_const_vector(h, t, Ψx, Cw):
+    if ((timestamp == None) or
+        ((timestamp != None) and (timestamp >= RWU_start_hour and timestamp <= RWU_end_hour))):
+        _K = K(h[1:-1])
+        Rp = Rmin*np.exp((-Ψx/d)**Brwu)
+        λ = 1/(sum([β**(i*dz/Z) for (i,_) in enumerate(h[1:-1])]))
+        Rr = np.array([Rp/(λ*β**(i*dz/Z)) for (i,_) in enumerate(h[1:-1])])
+        Rsr = np.array([dz/(λ*β**(i*dz/Z)*_K[i]) for (i,_) in enumerate(h[1:-1])])
+        RWU = (h[1:-1] - Ψx)/(Rr + Rsr)
+        return RWU
+    else:
+        return np.zeros_like(h[1:-1])
+
+def create_const_vector(h, t, Ψx, Cw, timestamp):
     A = log_mean_p(K(h))*dt/(Cw[1:-1]*dz**2)
     B = log_mean_m(K(h))*dt/(Cw[1:-1]*dz**2)
     b = np.zeros_like(h)
-    RWU = compute_RWU(h, Ψx)
+    RWU = compute_RWU(h, Ψx, timestamp)
     b[1:-1] = h[1:-1] + (A - B)*dz - (RWU*(1/Cw[1:-1]) if sol_use_RWU else 0)
     b[0] = h[0]
     b[-1] = h[-1]
@@ -148,16 +155,22 @@ def create_coeff_matrix(h, Cw):
 
 def lower_boundary_condition(h):
     if sol_use_free_drainage_bc:
-        return h[-2] + dz*(gbc - 1) #neuman 
+        return h[-2] + dz*(gbc - 1) # Neuman
     else:
-        return h[-1] #dirichlet 
+        return h[-1] # Dirichlet
 
-def upper_boundary_condition(r=None):
-    return h0[0] if r == None else r # Dirichlet
+def upper_boundary_condition(h, rain=None):
+    if rain == None:
+        return h[0]
+    else:
+        θ0 = θ(h[0])
+        if rain > 0:
+            assert True, ''
+        return invθ(max(θdry, θ0 - dt*a1 + dt*a2*rain*(1/θ0)))
 
-def try_run_solver(h, t, Ψx, Cw):
+def try_run_solver(h, t, Ψx, Cw, timestamp=None):
     try:
-        return spsolve(create_coeff_matrix(h, Cw), create_const_vector(h, t, Ψx, Cw))
+        return spsolve(create_coeff_matrix(h, Cw), create_const_vector(h, t, Ψx, Cw, timestamp))
     except Exception as e:
         print(f'>> Solution didn\'t converge (t={t})')
         print(f'>> {e}')
@@ -178,17 +191,17 @@ if sol_do_calibration_workflow:
             current_checkpoint = __tchecks.pop() if len(__tchecks) > 0 else 1e99
 
         hm = h.copy()
-        hm[0] = upper_boundary_condition()
+        hm[0] = upper_boundary_condition(hm)
         hm[-1] = lower_boundary_condition(hm)
 
         hi = try_run_solver(hm, t, Ψx, Cw(h))
-        hi[0] = upper_boundary_condition()
+        hi[0] = upper_boundary_condition(hi)
         hi[-1] = lower_boundary_condition(hi)
         h = try_run_solver(hm, t, Ψx, Cw((hi + hm)/2))
 
         # |Ax - b| == residual
         if sol_compute_residuals:
-            residual = np.abs(np.asarray(np.matmul(create_coeff_matrix(h, Cw(h)).todense(), h)).reshape(-1) - create_const_vector(h, t, Ψx, Cw(h)))
+            residual = np.abs(np.asarray(np.matmul(create_coeff_matrix(h, Cw(h)).todense(), h)).reshape(-1) - create_const_vector(h, t, Ψx, Cw(h), None))
 
         if sol_compute_numerical_probes:
             if t >= current_probe_sp:
@@ -205,6 +218,7 @@ if sol_do_calibration_workflow:
 
     print(f'>> Computation took {round(end_time - start_time)} s')
 
+    # Safe to remove: the following 2 lines simplify working on the chart without having to execute the full solver.
     pd.to_pickle((h, all_h, np_surface_head, np_residual_median, np_residual_max), './data.pkl')
     (h, all_h, np_surface_head, np_residual_median, np_residual_max) = pd.read_pickle('./data.pkl')
 
@@ -243,7 +257,6 @@ if sol_do_calibration_workflow:
     ax3.set_ylim([Z, 0])
     ax3.set_xlabel('θ $(\\frac{cm^3}{cm^3})$')
     ax3.set_ylabel('z (m)')
-   # ax3.plot([θr, θr], [z[0], z[-1]], label='Residual', color='red', linestyle='--', linewidth=0.5)
     ax3.plot([θs, θs], [z[0], z[-1]], label='Saturation', color='blue', linestyle='--', linewidth=0.5)
     ax3.legend()
 
@@ -288,7 +301,7 @@ else:
     print(f'>> Running full model with input file {sol_datafile}')
     print(f'>> Simulating {round(ds_rain.shape[0]/2)} hours-clock')
     start_time = time()
-    for i,(timestamp, rain, m_Ψx) in enumerate(zip(ds_domain, ds_rain, ds_Ψx)): #reads rain from data 
+    for i,(timestamp, rain, m_Ψx) in enumerate(zip(ds_domain, ds_rain, ds_Ψx)):
         print(f'[{round(100*i/ds_rain.shape[0])}%] Timestamp: {timestamp}, Rain (mm): {rain}')
         t = 0
         τ = 3600/2
@@ -296,11 +309,11 @@ else:
         int_rwu = 0
         while t <= τ:
             hm = h.copy()
-            hm[0] = upper_boundary_condition(invθ(min(rain, θs/2)) if rain != 0 else invθ(0.1)) #rain 
+            hm[0] = upper_boundary_condition(hm, rain)
             hm[-1] = lower_boundary_condition(hm)
 
             hi = try_run_solver(hm, t, m_Ψx, Cw(h))
-            hi[0] = upper_boundary_condition(invθ(min(rain, θs/2)) if rain != 0 else invθ(0.1)) #θdry
+            hi[0] = upper_boundary_condition(hi, rain)
             hi[-1] = lower_boundary_condition(hi)
             h = try_run_solver(hm, t, m_Ψx, Cw((hi + hm)/2))
 
@@ -308,7 +321,7 @@ else:
                 np_surface_head.append(h[np_probe_depth])
                 current_probe_sp += np_probe_every_s
 
-            int_rwu += np.abs(np.sum(compute_RWU(h, m_Ψx)))
+            int_rwu += np.abs(np.sum(compute_RWU(h, m_Ψx, timestamp)))
             t += dt
         np_RWU.append(int_rwu)
         np_dΨx.append(last_Ψx - m_Ψx)
@@ -320,9 +333,6 @@ else:
 
     pd.to_pickle((np_surface_head, np_RWU), './data.pkl')
     (np_surface_head, np_RWU) = pd.read_pickle('./data.pkl')
-
-    # # Compute T (transpiration)
-    # transpiration = np.array(np_RWU) -
 
     extended_domain = pd.to_datetime(np.linspace(from_date.value, to_date.value, len(np_surface_head)))
 
@@ -343,7 +353,7 @@ else:
     ax2.set_xlim([from_date, to_date])
     # ax2.set_ylim([0, 10000])
     ax2.plot(ds_domain, ds_sapflow, linewidth=0.5, label='Sapflow (Measurement)')
-    ax2.plot(ds_domain, np.array(np_RWU), linewidth=0.5, label='RWU (Model)') #factor de escala 500 a array 
+    ax2.plot(ds_domain, np.array(np_RWU), linewidth=0.5, label='RWU (Model)')
     # ax2.plot(ds_domain, np.array(np_RWU) - np.array(np_dΨx)*C, linewidth=0.5, label='Sapflow (Model)')
     ax2.grid()
     ax2.legend()

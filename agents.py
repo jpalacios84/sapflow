865871665d7201a20b852461231cf2fe01e3c779
Estefania <101481973+estefaniarn@users.noreply.github.com>
2023-01-30 21:24:20 -0700

    Jan30

--- agents.py
+++ agents.py
@@ -6,16 +6,16 @@ S_IN_DAY = 24*60*60
 
 sm = {
     'crack' : {
-        'vol' : 1,
+        'vol' : 5,
         'ts_vol' : [None]*S_IN_DAY,
     },
 
     'plant' : {
-        'vol' : 1,
+        'vol' : 0.5,
         'ts_vol' : [None]*S_IN_DAY,
     },
 
-    'rain'          : 1/S_IN_DAY,
+    'rain'          : 5/S_IN_DAY,
     'snowmelt'      : 1/S_IN_DAY,
     'RWU_crack'     : 1/S_IN_DAY,
     'free_drainage' : 0.75/S_IN_DAY,
@@ -23,7 +23,7 @@ sm = {
 }
 
 for n in range(S_IN_DAY):
-    is_rwu_active = n < 12*60*60
+    is_rwu_active = n < 6*60*60
 
 
     # Crack <- Environment
@@ -59,8 +59,13 @@ for k in ['crack', 'plant']:
     ax.plot(sm[k]['ts_vol'], label=k.upper())
 
 ax.set_xlim([0, S_IN_DAY])
+<<<<<<< Updated upstream
 ax.set_ylim([0, 3])
 #ax.grid()
+=======
+ax.set_ylim([0, 5])
+ax.grid()
+>>>>>>> Stashed changes
 ax.legend()
 ax.set_xlabel('Time (s)')
 ax.set_ylabel('Volume (L)')
